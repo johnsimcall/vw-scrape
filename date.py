ffc81debf4625a735b5a1c826d5e08b45d09c6cf
John Call <johnsimcall@gmail.com>
2018-09-09 00:21:27 -0600

    test

--- date.py
+++ date.py
@@ -2,5 +2,11 @@
 
 import datetime
 
-print(datetime.datetime.now())
-print(datetime.datetime.now().strftime("%Y-%m-%d"))
+today = datetime.datetime.now().strftime("%Y-%m-%d")
+f = open(.join('asdf', '-', filename),'w',encoding='utf-8')
+
+print(datetime.datetime.now(), file=f)
+print(datetime.datetime.now().strftime("%Y-%m-%d"), file=f)
+
+f.close()
+
