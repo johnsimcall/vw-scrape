b39d3419d98f99afe7b7a4b885c6842e40eaa30f
John Call <johnsimcall@gmail.com>
2018-09-09 01:07:03 -0600

    refactor as function

--- date.py
+++ date.py
@@ -2,8 +2,11 @@
 
 import datetime
 
+filename = "testname"
 today = datetime.datetime.now().strftime("%Y-%m-%d")
-f = open(.join('asdf', '-', filename),'w',encoding='utf-8')
+filename += today + ".csv"
+
+f = open(filename,'w',encoding='utf-8')
 
 print(datetime.datetime.now(), file=f)
 print(datetime.datetime.now().strftime("%Y-%m-%d"), file=f)
--- scrape.py
+++ scrape.py
@@ -15,10 +15,14 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 
-#url="https://www.vwsouthtowne.com/new-vehicles/atlas/"; filename="vwsouthtowne.csv"; dealer="VWST"
-url="https://www.kengarffvw.com/new-vehicles/atlas/" ; filename="kengarff.csv"; dealer="KGVW"
+url="https://www.kengarffvw.com/new-vehicles/atlas/" ; filename="TEST-"; dealer="TEST"
+#url="https://www.vwsouthtowne.com/new-vehicles/atlas/"; filename="TEST-"; dealer="TEST"
+
+#url="https://www.vwsouthtowne.com/new-vehicles/atlas/"; filename="VWST-"; dealer="VWST"
+#url="https://www.kengarffvw.com/new-vehicles/atlas/" ; filename="KGVW-"; dealer="KGVW"
 
 today = datetime.datetime.now().strftime("%Y-%m-%d")
+filename += today + ".csv"
 f = open(filename,'w',encoding='utf-8')
 
 options = webdriver.ChromeOptions()
@@ -28,26 +32,20 @@ options.add_argument("incognito")
 driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=options)
 driver.get(url)
 
-soup = BeautifulSoup(driver.page_source, 'lxml')
-x = soup.find('table', attrs={"class" : "results_table"})
-for row in x.find_all('tr', attrs={"class" : "hidden-xs"}):
-    print(row.find('div', {'class' : 'vinstock'}).get_text(":", strip=True),end=" ")
-    print(row.find('div', {'class' : 'price-block our-price real-price'}).get_text(":", strip=True),end="")
-    print(' Dealer: ' + dealer + ' Date: ' + today)
-    f.write(row.find('div', {'class' : 'vinstock'}).get_text(":", strip=True))
-    print(' ', end="", file=f)
-    f.write(row.find('div', {'class' : 'price-block our-price real-price'}).get_text(":", strip=True))
-    print(' Dealer: ' + dealer + ' Date: ' + today + '\n', end="", file=f)
+def scrape():
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    x = soup.find('table', attrs={"class" : "results_table"})
+    for row in x.find_all('tr', attrs={"class" : "hidden-xs"}):
+        print(row.find('div', {'class' : 'vinstock'}).get_text(":", strip=True), end=":", file=f)
+        print(row.find('div', {'class' : 'price-block our-price real-price'}).get_text(":", strip=True), end=":", file=f)
+        print('Dealer:' + dealer + ':Date:' + today, end=":", file=f)
+        for detail in row.find("div",{"class": "options"}).find_all("span"):
+            if detail.string in ["Exterior:", "Interior:"]:
+                print(detail.get_text() + detail.find_next_sibling().get_text(), end=":", file=f)
+        trim = row.find("div",{"class": "vehicle list-view new-vehicle publish"}).get("data-trim")
+        print('Trim:' + trim, file=f)
 
-    # VIN: 1V2NR2CA8JC588837:Stock #: 81416 Price:$51,525
-    # VIN: 1V2AP2CA1JC595746:Stock #: 2W80638 GarffEase Price:$30,115
-    
-    trim = row.find("div",{"class": "vehicle list-view new-vehicle publish"}).get("data-trim")
-    print('Trim: ' + trim)
-    for detail in row.find("div",{"class": "options"}).find_all("span"):
-        if detail.string in ["Exterior:", "Interior:"]:
-            print(detail.get_text() + detail.find_next_sibling().get_text())
-
+scrape()
 
 while True:
     try:
@@ -61,48 +59,8 @@ while True:
         #print("Going to next page...")
         next_page_element.click()
         time.sleep(2)
-
-        soup = BeautifulSoup(driver.page_source, 'lxml')
-        x = soup.find('table', attrs={"class" : "results_table"})
-        for row in x.find_all('tr', attrs={"class" : "hidden-xs"}):
-            print(row.find('div', {'class' : 'vinstock'}).get_text(":", strip=True),end=" ")
-            print(row.find('div', {'class' : 'price-block our-price real-price'}).get_text(":", strip=True),end="")
-            print(' Dealer: ' + dealer + ' Date: ' + today)
-            f.write(row.find('div', {'class' : 'vinstock'}).get_text(":", strip=True))
-            print(' ', end="", file=f)
-            f.write(row.find('div', {'class' : 'price-block our-price real-price'}).get_text(":", strip=True))
-            print(' Dealer: ' + dealer + ' Date: ' + today + '\n', end="", file=f)
-            # VIN: 1V2NR2CA8JC588837:Stock #: 81416 Price:$51,525
-            # VIN: 1V2AP2CA1JC595746:Stock #: 2W80638 GarffEase Price:$30,115
-    
-            trim = row.find("div",{"class": "vehicle list-view new-vehicle publish"}).get("data-trim")
-            print('Trim: ' + trim)
-            for detail in row.find("div",{"class": "options"}).find_all("span"):
-                if detail.string in ["Exterior:", "Interior:"]:
-                    print(detail.get_text() + detail.find_next_sibling().get_text())
+        scrape()
 
 print("Finished while True: loop")
 driver.quit()
-
-
-
-
-
-#filter_element = driver.find_element_by_xpath("//a[@title='3.6L V6 SEL Premium']")
-
-#raw=driver.page_source
-#file = open('raw.source','w',encoding='utf-8')
-#file.write(raw)
-#file.close()
-
-        # Several terrible ideas to wait for the next page to load before looking for results
-        #try:
-        #    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "cn-pk-user")))
-        #except TimeoutException:
-        #    print("Timed out waiting for element to load...")
-        #    driver.quit()
-        #    quit()
-
-        #driver.until(ExpectedConditions.jsReturnsValue("return document.readyState==\"complete\";"))
-        #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        #driver.execute_script("window.scrollTo(0, 0);")
+f.close()
